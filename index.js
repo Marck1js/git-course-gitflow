1bee1a5be7a9b7696e12d153672a2b45d8f75932
Marco <marckk63prog@gmail.com>
2022-06-20 12:52:09 -0300

    Nueva Ruta About: Usando Express.js, MJ

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get('/', (req,res)=>{
     res.send({msg: "Welcome To 'Git-Course' proyect"})
 })
 
+app.get('/about', (req,res)=> {
+    res.send({msg: "Estas en otra ruta llamada: '/about' "})
+})
+
 
 app.listen(3001, ()=>{
     console.log('Escuchando en puerto 3001')
